c2df5072439b3d2b8572352c9ce0720dc59c521c
Héléna Ganeman-Valot <helena.ganeman-valot@epitech.eu>
2021-10-15 17:24:47 +0200

    Fixed audio for blocks & items

--- Assets/Scripts/Healths/PlayerHealth.cs
+++ Assets/Scripts/Healths/PlayerHealth.cs
@@ -26,6 +26,7 @@ public class PlayerHealth : Health
     
     public void GetBonus()
     {
+        print("Test");
         if (life == 1) {
             life++;
             _spriteRenderer.sprite = _baseSprite;
